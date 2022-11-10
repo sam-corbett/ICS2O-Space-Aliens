0e50f4e97e5aefbdf95c076a9c9ef735627dde00
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-11-10 12:25:33 +0000

    added config file

---
+++ js/game.js
@@ -0,0 +1,16 @@
+/* global Phaser */
+
+// Copyright (c) 2022 Sam Corbett All rights reserved
+//
+// Created by: Sam Corbett
+// Created on: Nov 2022
+// This is the Phaser3 game configuration file!
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a
+};
+const game = new Phaser.Game(config);
+console.log(game);
