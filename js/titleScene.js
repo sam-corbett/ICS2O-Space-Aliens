6b1b799e042cb84dc376a36f2c736c430a249de1
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-11-17 19:44:45 +0000

    title scene brief changes

---
+++ js/titleScene.js
@@ -0,0 +1,55 @@
+/* Global Phaser */
+
+// Copyright (c) 2022 Sam Corbett All rights reserved
+//
+// Created by: Sam Corbett
+// Created on: Nov 2022
+// This is the Title Scene!
+
+/**
+ * Class Is In Title Scene.
+ */
+class TitleScene extends Phaser.Scene {
+  /**
+   * This Method Is The Constructor.
+   */
+
+  constructor() {
+    super({ key: "titleScene" });
+  }
+  /**
+   * Can be defined on your own scenes.
+   * Method called by Scene Manager when scene starts,
+   * before preload() & create().
+   */
+  init(data) {
+    this.cameras.main.setBackgroundColor("#ffffff");
+  }
+  /**
+   * Can be defined on your own scenes.
+   * Use it to load scenes.
+   */
+  preload() {
+    console.log("Title Scene");
+  }
+
+  /**
+   * Can be defined on your own scenes.
+   * Use it to make your game objects.
+   */
+  create(data) {
+    //pass
+  }
+
+  /**
+   * Should be overridden by your scenes.
+   * Method gets called once per game step while scene is running.
+   * @param {number} time - The Current Time
+   * @param {number} delta - The delta time in ms since the last frame.
+   */
+  update(time, delta) {
+    //pass
+  }
+}
+
+export default TitleScene;
