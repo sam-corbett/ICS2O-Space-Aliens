bc44c9ad2b1736efbb06c01c3555590d45d62fd5
Sam Corbett <113055935+sam-corbett@users.noreply.github.com>
2022-12-05 19:52:47 +0000

    fireMissile sprite and code added

--- js/gameScene.js
+++ js/gameScene.js
@@ -19,6 +19,7 @@ class GameScene extends Phaser.Scene {
 
     this.background = null;
     this.ship = null;
+    this.fireMissile = false;
   }
 
   /**
@@ -38,6 +39,7 @@ class GameScene extends Phaser.Scene {
 
     this.load.image("starBackground", "./assets/starBackground.png");
     this.load.image("spaceShip", "./assets/spaceShip.png");
+    this.load.image("missile", "./assets/missile.png")
   }
 
   /**
@@ -49,8 +51,9 @@ class GameScene extends Phaser.Scene {
     this.background.setOrigin(0, 0);
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "spaceShip");
-  }
 
+    this.missleGroup = this.physics.add.group()
+  }
   /**
    * Should be overridden by your scenes.
    * Method gets called once per game step while scene is running.
@@ -60,6 +63,7 @@ class GameScene extends Phaser.Scene {
   update(time, delta) {
     const keyLeftObj = this.input.keyboard.addKey("LEFT");
     const keyRightObj = this.input.keyboard.addKey("RIGHT");
+    const keySpaceObj = this.input.keyboard.addKey("SPACE")
 
     if (keyLeftObj.isDown === true) {
       this.ship.x = this.ship.x - 5;
@@ -73,6 +77,19 @@ class GameScene extends Phaser.Scene {
       if (this.ship.x > 1920) {
         this.ship.x = 1920;
       }
+    }
+
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile === false) {
+        // fire missle
+        this.fireMissile = true
+        const aNewMissle = this.physics.add.sprite(this.ship.x, this.ship.y., "missile")
+        this.missleGroup.add(aNewMissile)
+      }
+    }
+
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
     }
   }
 }
